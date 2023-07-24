8ea00fae00c9433b3711800f56d26676f0e36a0f
ingointo <vishnuvishnumohanms@gmail.com>
2023-07-24 18:10:36 +0530

    Changes

---
+++ src/components/screens/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const Login = () => {
+
+
+    return (
+     
+            <View>
+                <Text>
+                    Login Form
+                </Text>
+            </View>
+      
+
+    )
+}
+
+export default Login
